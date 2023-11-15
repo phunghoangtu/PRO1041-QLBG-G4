2bde9c7afc5d562b69dcb60567b427b4aff65227
phunghoangtu <tuphph3112200@gmail.com>
2023-11-15 12:15:44 +0700

    update menu

--- src/main/java/com/g4/swing/Menu.java
+++ src/main/java/com/g4/swing/Menu.java
@@ -92,7 +92,7 @@ public class Menu extends javax.swing.JPanel {
     protected void paintChildren(Graphics grphcs) {
         Graphics2D g2 = (Graphics2D) grphcs;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        GradientPaint g = new GradientPaint(0, 0, Color.decode("#CAC531"), 0, getHeight(), Color.decode("#CAC531"));
+        GradientPaint g = new GradientPaint(0, 0, Color.decode("#1CB5E0"), 0, getHeight(), Color.decode("#000046"));
         g2.setPaint(g);
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
         g2.fillRect(getWidth() - 20, 0, getWidth(), getHeight());
